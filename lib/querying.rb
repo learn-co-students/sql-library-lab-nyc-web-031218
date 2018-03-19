8fd118a655e0d0f409331bf4814ec48fd24d06f0
harim1206 <harim1206@gmail.com>
2018-03-19 16:15:01 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,78 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "
+  SELECT books.title, books.year
+  FROM books INNER JOIN series
+  ON books.series_id = series.id
+  WHERE series.id = \"1\"
+  ORDER BY books.year
+
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "
+  SELECT name, motto
+  FROM characters
+  ORDER BY motto
+  LIMIT 1
+
+
+  "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "
+  SELECT species, COUNT (species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT (species) DESC
+  limit 1
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "
+  SELECT authors.name, subgenres.name
+  FROM
+    authors
+    INNER JOIN series
+      ON authors.id = series.author_id
+    INNER JOIN subgenres
+      ON series.subgenre_id = subgenres.id
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "
+
+  SELECT series.title
+  FROM characters INNER JOIN series
+  ON characters.series_id = series.id
+  WHERE species = \"human\"
+  GROUP BY series_id
+  ORDER BY count(species) DESC
+  LIMIT 1
+
+
+  "
+  # SELECT series.title, characters.species, count(characters.species), characters.series_id
+  # FROM characters INNER JOIN series
+  # ON characters.series_id = series.id
+  # WHERE species = "human"
+  # GROUP BY series_id
+  # ORDER BY count(species) DESC
+  # LIMIT 1
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "
+  SELECT characters.name, count(character_books.book_id)
+  FROM characters INNER JOIN character_books
+  ON characters.id = character_books.character_id
+  GROUP BY character_books.character_id
+  ORDER BY count(character_books.book_id) DESC, characters.series_id DESC, characters.id DESC
+  "
+
 end
