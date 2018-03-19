a072eee654a1b6b24248d7b8aa6d8f7e214fa8ae
Eric Kim <hyeokjungkim@gmail.com>
2018-03-19 15:46:27 -0400

    Done.

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,2 @@
-UPDATE characters SET species = "Martian" WHERE id = (SELECT characters.id FROM characters ORDER BY id DESC LIMIT 1)
+UPDATE characters SET species = "Martian" WHERE id = (
+  SELECT characters.id FROM characters ORDER BY id DESC LIMIT 1)
